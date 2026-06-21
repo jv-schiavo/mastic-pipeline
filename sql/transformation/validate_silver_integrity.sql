48e414f9887ddda4c00d2b8084bae0270c45abef
joao victor schiavo <jvgs.schiavo@gmail.com>
2026-06-21 21:15:58 +0100

    Moved validation into a separate folder

--- sql/transformation/validate_silver_integrity.sql
+++
@@ -1,6 +0,0 @@
--- Basic row counts
-SELECT COUNT(*) AS client_count FROM silver.clients;
-SELECT COUNT(*) AS site_count FROM silver.sites;
-SELECT COUNT(*) AS job_count FROM silver.jobs;
-SELECT COUNT(*) AS employee_count FROM silver.employees;
-SELECT COUNT(*) AS material_count FROM silver.materials;
